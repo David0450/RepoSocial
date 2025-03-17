7d7a5de847950d3d10e1a88d9b2a3b4a3c762f04
David <450@cifpceuta.es>
2025-03-17 14:49:36 +0100

    .htaccess created

--- App/index.php
+++ App/index.php
@@ -6,8 +6,10 @@ session_start();
 require __DIR__ . '/Core/Router.php';
 require __DIR__ . '/Core/Database.php';
 require __DIR__ . '/Core/EmptyModel.php';
+require __DIR__ . '/Controllers/ProductsController.php';
 
 $router = new Router();
+
 
 $router->run();
 
